99faf63eff5440bc8bc5ffb816a3850e1faccc61
Markus Ressel <mail@markusressel.de>
2021-01-24 21:11:26 +0100

    added alertmanager file for api calls added /alerts command print a simple list of alerts when calling /alerts added telegram-click-aio dependency

--- alerticular/telegram.py
+++ alerticular/telegram.py
@@ -4,6 +4,9 @@ from typing import Any, Dict
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils.emoji import emojize
 from jinja2 import Environment, PackageLoader
+from telegram_click_aio.decorator import command
+
+from alerticular import alertmanager
 
 logger = logging.getLogger(__name__)
 JSONType = Dict[str, Any]
@@ -23,6 +26,7 @@ def setup(token: str) -> None:
     bot = Bot(token=token)
     dispatcher = Dispatcher(bot)
     dispatcher.register_message_handler(send_welcome, commands=["start", "help"])
+    dispatcher.register_message_handler(handle_alerts, commands=["alerts", "a"])
     dispatcher.register_message_handler(echo)
 
 
@@ -36,6 +40,22 @@ async def send_welcome(message: types.Message) -> None:
     await message.reply(
         "This bot is alerticular good!\nYour chat ID is: `{}`".format(message.chat.id), parse_mode="Markdown"
     )
+
+
+@command(name=["alerts", "a"], description='Show a list of all alerts.')
+async def handle_alerts(message: types.Message) -> None:
+    logger.info("{}: {}".format(message.chat, message.text))
+    alerts = await alertmanager.get_alerts()
+    lines = []
+    for alert in alerts:
+        alert_name = alert.get("labels", {}).get("alertname", None)
+        alert_message = alert.get("annotations", {}).get("message", None)
+        if alert_name is not None:
+            lines.append(f":fire: {alert_name}: {alert_message}")
+    text = "\n".join(lines).strip()
+    if len(text) <= 0:
+        text = "No alerts right now"
+    await message.reply(emojize(text), parse_mode="Markdown", disable_web_page_preview=True)
 
 
 async def echo(message: types.Message) -> None:
